194060b7acb5bae22a3a30c1f540191e62ae0c6e
kyrakura <caidavid13@gmail.com>
2023-10-16 22:56:11 +0300

    lab 4.6 commit 2

--- lab 4.6/lab 4.6.cpp
+++ lab 4.6/lab 4.6.cpp
@@ -3,80 +3,63 @@
 using namespace std;
 
 int main() {
-    double P, S, pt;
-    int n, i, k;
+    double P, S;
+    int n = 0, i, k;
 
     // 1) while(…) {… while(…) {…} …};
-    P = 0;
-    n = 1;
-    while (n <= 13) {
-        S = 0;
-        i = 1;
-        while (i <= n) {
-            pt = 1; 
-            k = 1;
-            while (k <= i) {
-                pt *= sqrt(k);
-                k++;
+    S = 0;
+    i = 1;
+    while (i <= 15) {
+        P = 1;
+        k = 1;
+        while (k <= i) {
+            P *= sqrt(k);
+            k++;
             }
-            S += (sin(10 * i) + cos(10 * i)) / pt; 
-            i++;
-        }
-        P += S;
-        n++;
+        S += (sin(10 * i) + cos(10 * i)) / P;
+        i++;
     }
-    cout << "while: " << P << endl;
+    cout << "while: " << S << endl;
 
     // 2) do{… do{…} while(…) …} while(…);
-    P = 0;
-    n = 1;
+    S = 0;
+    i = 1;
     do {
-        S = 0;
-        i = 1;
+        P = 1;
+        k = 1;
         do {
-            pt = 1; 
-            k = 1;
-            do {
-                pt *= sqrt(k);
-                k++;
-            } while (k <= i);
-            S += (sin(10 * i) + cos(10 * i)) / pt; 
+            P *= sqrt(k);
+            k++;
+        } while (k <= i);
+            S += (sin(10 * i) + cos(10 * i)) / P;
             i++;
-        } while (i <= n);
-        P += S;
-        n++;
-    } while (n <= 13);
-    cout << "do-while: " << P << endl;
+        } while (i <= 15);
+       
+    cout << "do-while: " << S << endl;
 
     // 3) for(…; …; n++) {… for(…; …; k++) {…} …};
-    P = 0;
-    for (n = 1; n <= 13; n++) {
-        S = 0;
-        for (i = 1; i <= n; i++) {
-            pt = 1;
-            for (k = 1; k <= i; k++) {
-                pt *= sqrt(k);
+    S = 0;
+    for (i = 1; i <= 15; i++) {
+        P = 1;
+        for (k = 1; k <= i; k++) {
+            P *= sqrt(k);
             }
-            S += (sin(10 * i) + cos(10 * i)) / pt;
+            S += (sin(10 * i) + cos(10 * i)) / P;
         }
-        P += S;
-    }
-    cout << "for ++: " << P << endl;
+   
+    cout << "for ++: " << S << endl;
 
     // 4) for(…; …; n--) {… for(…; …; k--) {…} …}.
-    P = 0;
-    for (n = 13; n >= 1; n--) {
-        S = 0;
-        for (i = n; i >= 1; i--) {
-            pt = 1;
-            for (k = i; k >= 1; k--) {
-                pt *= sqrt(k);
+    S = 0;
+    for (i = 15; i >= 1; i--) {
+        P = 1;
+        for (k = i; k >= 1; k--) {
+            P *= sqrt(k);
             }
-            S += (sin(10 * i) + cos(10 * i)) / pt;
+            S += (sin(10 * i) + cos(10 * i)) / P;
         }
-        P += S;
-    }
-    cout << "for --: " << P << endl;
+    cout << "for --: " << S << endl;
 
     return 0;
 }
+
